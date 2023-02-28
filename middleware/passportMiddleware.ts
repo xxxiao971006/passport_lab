38635ed9447fcb1cd951bb2450337cd3b5cfde12
xxxiao971006 <114138744+xxxiao971006@users.noreply.github.com>
2023-02-27 21:27:25 -0800

    Update passportMiddleware.ts

--- middleware/passportMiddleware.ts
+++ middleware/passportMiddleware.ts
@@ -1,16 +1,69 @@
-import { Application } from "express";
-import passport from "passport";
-import PassportConfig from "./PassportConfig";
+import { Strategy as GitHubStrategy } from 'passport-github2';
+import { PassportStrategy } from '../../interfaces/index';
+import passport from 'passport';
+import { getUserById } from "../../controllers/userController";
+import { database } from '../../models/userModel';
 
-import localStrategy from "./passportStrategies/localStrategy";
-import passportGitHubStrategy from "./passportStrategies/githubStrategy";
+declare global {
+    namespace Express {
+        interface User {
+            id?: number | undefined;
+            name: string;
+            email: string;
+            password: string;
+            role: string;
+        }
+    }
+}
 
-// No need to actually pass the instance of passport since it returns a singleton
-const passportConfig = new PassportConfig();
-passportConfig.addStrategies([localStrategy /* passportGitHubStrategy */]);
-const passportMiddleware = (app: Application): void => {
-  app.use(passport.initialize());
-  app.use(passport.session());
+passport.serializeUser(function (user: Express.User, done: (err: any, id?: number) => void) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id: number, done: (err: any, user?: Express.User | null | false) => void) {
+    let user = getUserById(id);
+    if (user) {
+        done(null, user);
+    } else {
+        done({ message: "User not found" }, null);
+    }
+});
+
+const githubStrategy: GitHubStrategy = new GitHubStrategy(
+    
+    {
+        clientID: "f2187bf0e81f9d37f843",
+        clientSecret: "b0eb137030029d538dda6f952f7f08aa7679bec3",
+        callbackURL: "http://localhost:8000/auth/github/callback", // where github hands in the user profile information
+        passReqToCallback: true,
+
+    },
+    
+    /* FIX ME 😭 */
+    async function (req: any, accessToken: any, refreshToken: any, profile: any, done: any) {
+        console.log(profile);
+        database.push({
+            id: Number([profile.id]),
+            name: profile.username,
+            email: "ABC@gmail.com",
+            password: "ABC123!",
+            role: "guest"
+        },)
+        let user = getUserById(Number(profile.id));
+        if (user) {
+            done(null, user);
+        } else {
+            done({ message: "User not found" }, null);
+        }
+        console.log(user)
+    }    
+);
+
+
+const passportGitHubStrategy: PassportStrategy = {
+    name: 'github',
+    strategy: githubStrategy,
 };
 
-export default passportMiddleware;
+export default passportGitHubStrategy;
+
